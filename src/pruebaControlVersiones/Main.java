19864713aa750af67b4019a68789b9be246057ef
Oleglukyyy <oluk857@g.educaand.es>
2025-12-10 09:13:28 +0100

    Version E: Implementacion de funcion factorial con bucle for

--- src/pruebaControlVersiones/Main.java
+++ src/pruebaControlVersiones/Main.java
@@ -36,5 +36,14 @@ public class Main {
 
 	    return num;
 	}
-
+	
+	private static long factorialBucle(int n) {
+		int resultado=1;
+		for (int i =n;i>0;i--) {
+			resultado*=i;
+		}
+		return resultado;
+	}
+	
+	
 }
