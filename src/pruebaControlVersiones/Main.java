dab69901534822d1b1628d8f7bfb03dba5a3d124
Oleglukyyy <oluk857@g.educaand.es>
2025-12-10 08:45:24 +0100

    Version B: Implementacion de funcion para pedir numero a usuario

--- src/pruebaControlVersiones/Main.java
+++ src/pruebaControlVersiones/Main.java
@@ -1,23 +1,26 @@
 package pruebaControlVersiones;
 
+import java.util.Scanner;
+
 public class Main {
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Scanner sc = new Scanner(System.in);
-
-        System.out.print("Introduce un número entero mayor o igual que 0: ");
-        int num = sc.nextInt();
-
+		int num = pedirNumero();
         if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
         } else {
             System.out.println("Has introducido el número: " + num);
         }
 
-        sc.close();
+       
 		
+	}
+	public static int pedirNumero() {
+
+        System.out.print("Introduce un número entero mayor o igual que 0: ");
+        return sc.nextInt();
 	}
 
 }
